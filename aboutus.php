a1b1e715af219e8a8803fbd789628684ea4f7317
Sid Lohar <loharsid1111@gmail.com>
2023-06-07 15:38:09 +0530

    First commit

---
+++ aboutus.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+ 
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="shortcut icon" type="image/png" href="assets/img/P.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/user.css">
+    <link rel="stylesheet" href="assets/w3css/w3.css">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation" style="color: black">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                    </button>
+                <a class="navbar-brand page-scroll" href="index.php">
+                   Car Booking </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+
+            <?php
+                if(isset($_SESSION['login_client'])){
+            ?> 
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_client']; ?></a>
+                    </li>
+                    <li>
+                    <ul class="nav navbar-nav navbar-right">
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Control Panel <span class="caret"></span> </a>
+                <ul class="dropdown-menu">
+              <li> <a href="entercar.php">Add Car</a></li>
+              <li> <a href="enterdriver.php"> Add Driver</a></li>
+             
+              <li> <a href="clientview.php">View</a></li>
+
+            </ul>
+            </li>
+          </ul>
+                    </li>
+                    <li>
+                        <a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
+                    </li>
+                </ul>
+            </div>
+            
+            <?php
+                }
+                else if (isset($_SESSION['login_customer'])){
+            ?>
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_customer']; ?></a>
+                    </li>
+                    <ul class="nav navbar-nav">
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Garagge <span class="caret"></span> </a>
+                <ul class="dropdown-menu">
+                <li> <a href="prereturncar.php">Return Now</a></li>
+              <li> <a href="mybookings.php"> My Bookings</a></li>
+            </ul>
+            </li>
+          </ul>
+                    <li>
+                        <a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
+                    </li>
+                </ul>
+            </div>
+
+            <?php
+            }
+                else {
+            ?>
+
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="clientlogin.php">Employee</a>
+                    </li>
+                    <li>
+                        <a href="customerlogin.php">Customer</a>
+                    </li>
+                    <li>
+                        <a href="faq/index.php"> About Us </a>
+                    </li>
+                </ul>
+            </div>
+                <?php   }
+                ?>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+     
+    <div class="content">
+				<div class="container_12">
+					<div class="grid_12">
+						<br><br><br><br>
+                       <center> <h2>About Us</h2> </center>
+                       <br><br><br><br>
+						
+						
+
+
+
+
+					</div>
+					<div class="grid_5">
+						
+						<center>
+							<div class="text1 color2">Group M12</div>
+							<p>Don’t forget, 24/7 support is available from Group M12  <span class="color1"></span> </p>
+						
+							<address>
+								<dl>
+									<dt>Chandan Nehete <br> Siddhesh Lohar <br> Dipali Tayade <br> Apurv Wani <br>
+										KCE'S College of Engineering And Management,<br>
+										Jalgaon, Maharashtra 425001
+									</dt>
+									<dd><span>Telephone:</span> +91 0000000000</dd>
+									<dd><span>FAX:</span> +91 0000000000</dd>
+									<dd>E-mail: <a href="#" class="color1">groupM12@gmail.com</a></dd>
+								</dl>
+							</address>
+                </center>
+						</div>
+					</div>
